0fd853c53e2ba6c4cc96346956b5bc8175e07770
elik <elik@example.com>
2021-01-22 13:11:16 +0200

    Load Connect to DB files

---
+++ BaraholkaTeam.DAL/ContextData/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BaraholkaTeam.DAL.ContextData
+{
+    [Table("tblUsersTeamProject")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [Required, StringLength(255)]
+        public string Surname { get; set; }
+        [Required, StringLength(255)]
+        public string Login { get; set; }
+        [Required, StringLength(255,MinimumLength = 6)]
+        public string Password { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
