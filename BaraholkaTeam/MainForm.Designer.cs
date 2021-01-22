e116949fc928401e47fb24e60837055d1e97fe66
elik <elik@example.com>
2021-01-22 14:13:21 +0200

    Add method CreateGroupBox, and designed MainForm

--- BaraholkaTeam/MainForm.Designer.cs
+++ BaraholkaTeam/MainForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Drawing;
+using System.IO;
+
 namespace BaraholkaTeam
 {
     partial class MainForm
@@ -29,12 +31,104 @@ namespace BaraholkaTeam
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemProfile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemKorsina = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.toolStripMenuItemKorsina});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(800, 28);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemProfile,
+            this.toolStripMenuItemExit});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(59, 24);
+            this.toolStripMenuItemFile.Text = "Файл";
+            // 
+            // toolStripMenuItemProfile
+            // 
+            this.toolStripMenuItemProfile.Name = "toolStripMenuItemProfile";
+            this.toolStripMenuItemProfile.Size = new System.Drawing.Size(151, 26);
+            this.toolStripMenuItemProfile.Text = "Профіль";
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(151, 26);
+            this.toolStripMenuItemExit.Text = "Вийти";
+            // 
+            // toolStripMenuItemKorsina
+            // 
+            this.toolStripMenuItemKorsina.Name = "toolStripMenuItemKorsina";
+            this.toolStripMenuItemKorsina.Size = new System.Drawing.Size(83, 24);
+            this.toolStripMenuItemKorsina.Text = "Корзина";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSearch.Location = new System.Drawing.Point(96, 47);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PlaceholderText = "Пошук...";
+            this.txtSearch.Size = new System.Drawing.Size(494, 41);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(597, 47);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(107, 41);
+            this.btnConfirm.TabIndex = 2;
+            this.btnConfirm.Text = "Пошук";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.MaximumSize = new System.Drawing.Size(818, 500);
+            this.MinimumSize = new System.Drawing.Size(818, 500);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemProfile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemKorsina;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnConfirm;
     }
 }
